998cd3fb0bf33dd104a4231a4bc91c62af934999
rallysport206 <phan.tony125@gmail.com>
2017-12-04 22:43:51 -0800

    added delete buttons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-
 $('ul').on('click', 'li', function () {
+  $()
 })
+var deleteButton = "<button>X</button>";
 //submits what text is in the form
-$('#submit').click(function (e) {
-    e.preventDefault();
-  $('ul').append($('<li>',{
-    text: $('#form').val()
-  }));
+$('#submit').click(function () {
+  //var textInput = $('#form').val();
+  //console.log(textInput);
+  $('ul').append($('<li>' + $('#form').val() + deleteButton));
   $('#form').val('');
 });
+// $('#submit').click(function () {
+//   $('ul').append($('<li>',{
+//     text: $('#form').val()
+//   }));
+//   $('#form').val('');
+// });
 //suppose to remove value after submitting form
+//remove item from List
